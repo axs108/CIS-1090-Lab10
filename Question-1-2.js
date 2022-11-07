38bc43c025ec2c993b9a27c1a87acb24ba41349b
axs00458 <axs00458@castleton.edu>
2022-11-07 11:01:06 -0500

    Audreys Lab 10

--- Question-1-2.js
+++ Question-1-2.js
@@ -1,4 +1,9 @@
 //❓❓ LAB QUESTION 1, 2:
-let student = {};
+let student = {
+    firstName: "Audrey",
+    lastName: "Steinman",
+    classes: ["problem solving with computers", "popular literature", "intro to ethics", "intro to sociology", "drawing 1"]
+};
+
 
 console.log(student);
